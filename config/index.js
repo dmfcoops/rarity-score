2dec126b3fbd02432e0355b512456cdb52e9bbf8
dmfcoops <51507243+dmfcoops@users.noreply.github.com>
2022-05-17 11:20:45 +0100

    Update env and collection.json

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 export const config = {
-  env: "prod",
+  env: "local",
   LOCAL_API_URL: "localhost:3000",
   API_URL: "odp-rarity.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
-  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+  COLLECTION_NAME: "Pandimensionals",
+  COLLECTION_TITLE: "Pandimensionals",
+  COLLECTION_DESCRIPTION: "420 Pandimensional Weirdos",
+  COLLECTION_IMG_LINK:
+    "https://d3th0yrgxhrrkf.cloudfront.net/wp-content/uploads/2022/05/mcdoom-chieftain.jpg&nocache=1",
   STARTING_INDEX: 0,
 };
